52bad1f757886baaaf7df0b154df9f1cec9b12ac
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-08-26 15:42:06 -0400

    Update file duplicates

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,7 +3,7 @@ import './MovieCard.css'
 const MovieCard = ({handleClick, id, poster_path, title, release_date}) => {
   return (
     <div className='card' onClick={() => handleClick(id)} id={id}>
-      <img src={poster_path}/>
+      <img className='image' src={poster_path}/>
       <h2>{title}</h2>
       <h3>{release_date}</h3>
 
