ef324ac180c60fcfb83de95853bc28c8a23fdf52
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-08-28 20:26:40 -0400

    Add testing for homepage user flow.

---
+++ cypress/e2e/homepage.cy.js
@@ -0,0 +1,23 @@
+describe('landing page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: 'allMovies.json'
+    })
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should show all movies when on the homepage', () => {
+    cy.location('pathname').should('eq', '/')
+    cy.get('.card').first().within(() => {
+      cy.get('img[name=movie-poster]').should('exist')
+      cy.contains('h2', 'Wifelike')
+      cy.contains('h3', '2022')
+    })
+    cy.get('.card').last().within(() => {
+      cy.get('img[name=movie-poster]').should('exist')
+      cy.contains('h2', 'X')
+      cy.contains('h3', '2022')
+    })
+  })
+})
