26e18cdb3839de41bddac642568cfb46ad2640d7
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-08-29 17:59:56 -0400

    Begin testing fro single movie

--- cypress/e2e/singleMovie.cy.js
+++ cypress/e2e/singleMovie.cy.js
@@ -1,17 +1,23 @@
-describe('navigate to individual movie', () => {
-beforeEach(() => {
-  cy.intercept("GET",'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-    statusCode: 200,
-    fixture: 'singleMovie.json'
-  } )
-})
+describe("navigate to individual movie", () => {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        statusCode: 200,
+        fixture: "singleMovie.json",
+      }
+    ).visit("http://localhost:3000/");
+  });
 
-  it('should show user single movie with information', () => {
-    cy.visit('localhost:3000')
-  })
-})
+  it("should show user landing page with all movies, titles, and release dates", () => {
+    cy.get(".card-container").find(".card").should("have.length", 4);
+    // cy.get(".image")
+  });
+});
 
 // the user should see all movies displayed on homepage
 // the user will click on a single movie of their choice
 // the user will be taken to the single movies info page
-// the user will see the background photo, the 
+// the user will see the background photo, the description, the realease date, and the runtime
+// the user will see the Go Back Home button
