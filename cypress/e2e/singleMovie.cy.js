87846f6f293e821b1e6d72981e7a8f17885d9e92
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-08-29 20:42:50 -0400

    Create tests to navigate back to home page

--- cypress/e2e/singleMovie.cy.js
+++ cypress/e2e/singleMovie.cy.js
@@ -22,15 +22,21 @@ describe("navigate to individual movie", () => {
     )
     cy.get(".card-container").find(".card").should("have.length", 4);
     cy.get(".card").last().click()
-    // cy.contains('div', 'background-image')
     cy.contains('p', 'In 1979')
     cy.contains('p', '2022')
     cy.contains('p', 'Runtime:')
+    cy.contains('button', 'Go Back Home').click()
   });
-});
 
-// the user should see all movies displayed on homepage
-// the user will click on a single movie of their choice
-// the user will be taken to the single movies info page
-// the user will see the background photo, the description, the realease date, and the runtime
-// the user will see the Go Back Home button
+  it("should display the landing page to the user", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        statusCode: 200,
+        fixture: "allMovies.json",
+      }
+    )
+    cy.get(".card-container").find(".card").should("have.length", 4);
+  })
+});
