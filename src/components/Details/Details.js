2cccf1cbf9d0d9cb9477c3b0a2fef044fbea476a
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-01 10:06:04 -0400

    Resize info containers.

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,22 +1,28 @@
 import { Link } from 'react-router-dom';
 import arrow from '../../images/arrow.png';
 import Error from '../Error/Error';
+import './Details.css'
 
 const Details = ({ selectedMovieDetails, detailsError }) => {
+  const posterImage = selectedMovieDetails.poster_path;
+
   return (
     <div>
+      {console.log(selectedMovieDetails)}
       {detailsError ? (
         <Error />
       ) : ( 
-        <>
+        <div className='details-container'>
           <Link to="/">
             <img src={arrow} className='arrow' alt='back arrow icon'/>
           </Link>
-          <p className="title"></p>
-          <p>{selectedMovieDetails.overview}</p>
-          <p>{selectedMovieDetails.release_date}</p>
+          <img src={posterImage} className='poster' alt='movie-poster'/>
+          <h1 className="title">{selectedMovieDetails.title}</h1>
+          <h2 className="overview">{selectedMovieDetails.tagline}</h2>
+          <p className="overview">{selectedMovieDetails.overview}</p>
+          <p className="release-date">Released in {selectedMovieDetails.release_date}</p>
           <p>Runtime: {selectedMovieDetails.runtime} minutes</p>
-        </>
+        </div>
       )}
     </div>
   )
